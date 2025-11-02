023fd7f6b419c16cc6d8fcacf1738d890e87b4c1
sysid <sysid@gmx.de>
2025-11-02 15:10:11 +0100

    Bump version to 0.2.3

--- lua/bkmr/init.lua
+++ lua/bkmr/init.lua
@@ -8,7 +8,7 @@ local lsp = require('bkmr.lsp')
 local ui = require('bkmr.ui')
 
 -- Plugin version
-M.version = '0.2.2'
+M.version = '0.2.3'
 
 -- Setup function - configures the plugin
 function M.setup(opts)
